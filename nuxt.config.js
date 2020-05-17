b2f166dc5020d912431f6290645b2d76a7a09c94
ts3486 <taos3486@gmail.com>
2020-05-17 22:18:18 +0900

    first-commit

---
+++ nuxt.config.js
@@ -0,0 +1,15 @@
+// nuxt.config.js
+{
+  [
+    // Simple usage
+    "@nuxtjs/vuetify",
+
+    // With options
+    [
+      "@nuxtjs/vuetify",
+      {
+        /* module options */
+      },
+    ],
+  ];
+}
