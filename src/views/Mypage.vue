a4d32a7ea7c5991a859ccb55274af08c0bc1bca6
ts3486 <taos3486@gmail.com>
2020-05-29 19:42:59 +0900

    save

--- src/views/Mypage.vue
+++ src/views/Mypage.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>Mypage</h1>
+    <h1 class="MyPage">Profile</h1>
     <h3>ログイン成功しました</h3>
   </div>
 </template>
@@ -11,3 +11,17 @@ export default {
   data: () => {},
 };
 </script>
+
+<style lang="scss" scoped>
+@import url("https://fonts.googleapis.com/css2?family=Lobster&family=Merriweather:wght@300&display=swap");
+
+.MyPage {
+  font-family: "Lobster", cursive;
+  font-size: 60px;
+  display: flex;
+  justify-content: center;
+  margin-top: 50px;
+  padding-bottom: 30px;
+  border-bottom: 3px solid black;
+}
+</style>
