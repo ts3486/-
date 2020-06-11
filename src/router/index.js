13e34898f819e9f14c06064f2d0531615aaf9113
tsunga59 <tatsumochi15@gmail.com>
2020-06-11 14:33:55 +0900

    mypage更新

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,7 @@ import Home from "../views/Home.vue";
 import Musicians from "../views/Musicians.vue";
 import Login from "../views/Login.vue";
 import SignUp from "../views/Signup.vue";
-import MyPage from "../views/MyPage.vue";
+import MyPage from "../views/MyProfile.vue";
 
 Vue.use(VueRouter);
 
