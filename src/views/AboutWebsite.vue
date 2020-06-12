14141f218a2665d3db1dcee7bb0fc78968d3a609
ts3486 <taos3486@gmail.com>
2020-06-12 22:22:35 +0900

    graduation page

--- src/views/AboutWebsite.vue
+++ src/views/AboutWebsite.vue
@@ -1,9 +1,19 @@
 <template>
-  <div></div>
+  <div>
+    <div class="title">ミュージックアクセラレーターとは？</div>
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.title {
+  display: flex;
+  justify-content: center;
+  margin: 100px;
+  font-size: 1000;
+  font-weight: 1000;
+}
+</style>
--- src/views/GraduationPage.vue
+++ src/views/GraduationPage.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="graduationpage">
-    CONGRATULATIONS
+    <v-container class="container">
+      <v-img
+        alt="graduationimage"
+        class="graduationimage"
+        src="@/assets/graduation.jpg"
+      />
+    </v-container>
   </div>
 </template>
 
@@ -10,4 +16,42 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.graduationpage {
+  overflow-y: hidden;
+}
+
+.container {
+  position: relative;
+  text-align: center;
+}
+
+.graduationimage {
+  display: flex;
+  position: absolute;
+  justify-content: center;
+  width: 800px;
+  top: -135px;
+  right: 160px;
+}
+
+.container {
+  position: absolute;
+}
+
+.graduation {
+  position: relative;
+  right: -20px;
+  bottom: -125px;
+}
+
+// .box {
+//   position: relative;
+//   background: rgb(222, 222, 222);
+//   height: 150px;
+//   width: 400px;
+//   bottom: -220px;
+//   right: -420px;
+//   background-blend-mode: normal;
+// }
+</style>
